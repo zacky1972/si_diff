d11d52b6da9bdc2513f287d2f15f31eb6c60bd42
Susumu Yamazaki <zacky1972@gmail.com>
2022-01-23 10:04:55 +0900

    install and set up ex_doc

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule SiDiff.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/zacky1972/si_diff"
+  @version "0.1.0-dev"
+
   def project do
     [
       app: :si_diff,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -23,6 +27,15 @@ defmodule SiDiff.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "SiDiff",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
