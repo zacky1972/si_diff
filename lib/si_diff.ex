0bbd982f9815eff185c1e14da6ef0cc265222b04
Susumu Yamazaki <zacky1972@gmail.com>
2022-01-23 09:49:51 +0900

    initial commit

---
+++ lib/si_diff.ex
@@ -0,0 +1,18 @@
+defmodule SiDiff do
+  @moduledoc """
+  Documentation for `SiDiff`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SiDiff.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/si_diff_test.exs
@@ -0,0 +1,8 @@
+defmodule SiDiffTest do
+  use ExUnit.Case
+  doctest SiDiff
+
+  test "greets the world" do
+    assert SiDiff.hello() == :world
+  end
+end
