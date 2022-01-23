9a33c4ee24e0676dcf33484a2c04d8193574eba9
Susumu Yamazaki <zacky1972@gmail.com>
2022-01-23 10:16:19 +0900

    remove hello

--- lib/si_diff.ex
+++ lib/si_diff.ex
@@ -2,17 +2,4 @@ defmodule SiDiff do
   @moduledoc """
   `SiDiff` is "Satellite Image Diff".
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SiDiff.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/si_diff_test.exs
+++ test/si_diff_test.exs
@@ -1,8 +1,4 @@
 defmodule SiDiffTest do
   use ExUnit.Case
   doctest SiDiff
-
-  test "greets the world" do
-    assert SiDiff.hello() == :world
-  end
 end
